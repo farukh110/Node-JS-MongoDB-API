5aba9e5a2e72f410ac6b37d730e5e37817fa871f
farukh110 <farukhsajjad110@gmail.com>
2023-08-04 23:20:57 +0500

    initial step

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/books');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    } catch (error) {
+
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+app.get('/', (req, res) => {
+
+    res.send({ title: 'Books' });
+});
+
+app.get('/add-note', async (req, res) => {
+
+    try {
+
+        await Book.insertMany([
+            {
+                title: 'Sons of Tales',
+                body: 'content 1'
+            },
+            {
+                title: 'Need for Speed',
+                body: 'content 2'
+            }
+        ]);
+
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+
+});
+
+app.get('/books', async (req, res) => {
+
+    const book = await Book.find();
+
+    if (book) {
+
+        res.json(book);
+
+    } else {
+
+        console.log('something went wrong');
+    }
+});
+
+connectDB().then(() => {
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    })
+});
